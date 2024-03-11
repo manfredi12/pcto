630d88751566dfd1defc2d08a083ecaad5050221
manfredi12 <162984909+manfredi12@users.noreply.github.com>
2024-03-11 15:58:53 +0000

    .

---
+++ ex02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+
+{
+int a;
+int b;
+int c;
+int d;
+int e;
+int f;
+
+printf("inserisci un un numero\n");
+scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
+printf( "numeri inseriti %d\n", a+b+c+d+e+f);
+
+}
+scanf ("%d,&a)
+
